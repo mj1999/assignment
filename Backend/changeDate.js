081121ec553e7c39f96d2604398bcbdfdafc286c
mj1999 <mayank.joshi92@yahoo.com>
2023-12-21 21:36:08 +0530

    Add files via upload

---
+++ Backend/changeDate.js
@@ -0,0 +1,16 @@
+const siteData = require("../model/jobs");
+siteData.forEach((site, idx) => {
+  site.id = idx;
+});
+module.exports.changeDate = function (req, res) {
+  const siteId = req.body.id;
+  const newInstallationDate = req.body.date;
+  const site = siteData.find((s) => s.id == siteId);
+  if (site) {
+    console.log(site);
+    site.InstallationDate = newInstallationDate;
+    res.json({ message: "Installation date updated successfully" });
+  } else {
+    res.status(404).json({ message: "Site not found" });
+  }
+};
